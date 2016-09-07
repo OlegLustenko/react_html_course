dee4a16fe87b413017ce07e0be8b1b71cacda721
Oleg <oleg.lustenko@gmail.com>
2016-09-08 01:50:57 +0300

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+
+const config = {
+  entry: [
+    './src/client.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    contentBase: './',
+    inline: true
+  }
+};
+
+
+export default [config];
